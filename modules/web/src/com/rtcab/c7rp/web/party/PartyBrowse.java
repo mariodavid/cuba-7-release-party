83b781128fb03cfec50efbfea8be78aabb8aa737
Mario <mario@die-davids.de>
2019-01-28 08:39:54 +0100

    added start party message

--- modules/web/src/com/rtcab/c7rp/web/party/PartyBrowse.java
+++ modules/web/src/com/rtcab/c7rp/web/party/PartyBrowse.java
@@ -1,6 +1,23 @@
 package com.rtcab.c7rp.web.party;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.rtcab.c7rp.entity.Party;
+import javax.inject.Inject;
 
 public class PartyBrowse extends AbstractLookup {
+
+  @Inject
+  protected GroupTable<Party> partiesTable;
+
+
+  public void startParty() {
+
+    Party partyToStart = partiesTable.getSingleSelected();
+    String themeName = messages.getMessage(partyToStart.getTheme());
+    String startPartyMessage = formatMessage("startPartyMessage", partyToStart.getTitle(),
+        themeName);
+
+    showNotification(startPartyMessage, NotificationType.WARNING);
+  }
 }
