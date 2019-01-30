32bfc5c9dee15b6729afcf5eed5d23f614e5c80c
Mario <mario@die-davids.de>
2019-01-30 08:48:57 +0100

    party layout for CUBA 6

---
+++ modules/web/src/com/rtcab/c7rp/web/ExtAppMainWindow.java
@@ -0,0 +1,32 @@
+package com.rtcab.c7rp.web;
+
+import com.haulmont.cuba.gui.components.AbstractMainWindow;
+import com.haulmont.cuba.gui.components.Embedded;
+import com.haulmont.cuba.gui.components.mainwindow.FtsField;
+import com.haulmont.cuba.gui.components.mainwindow.SideMenu;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+
+public class ExtAppMainWindow extends AbstractMainWindow {
+    @Inject
+    private FtsField ftsField;
+
+    @Inject
+    private Embedded logoImage;
+
+    @Inject
+    private SideMenu sideMenu;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+
+        sideMenu.requestFocus();
+
+        initLayoutAnalyzerContextMenu(logoImage);
+        initLogoImage(logoImage);
+        initFtsField(ftsField);
+    }
+}
